6e2b0407f62169e4d0e272427be102a0c773bfd6
Christopher Terrano <mterrano1@yahoo.com>
2023-05-04 23:04:30 -0500

    create subtract test method

--- spec/math_operations_spec.rb
+++ spec/math_operations_spec.rb
@@ -8,4 +8,11 @@ describe MathOperations do
             expect(result.add(5, 2)).to eql(7)
         end
     end
+
+    describe '#subtract' do
+        it 'returns the difference of two numbers' do
+            result = MathOperations.new
+            expect(result.subtract(5, 2)).to eql(3)
+        end
+    end
 end
