a9515808ff1f59e2183f3977af10061972e2b725
Christopher Terrano <mterrano1@yahoo.com>
2023-05-04 23:11:28 -0500

    create multiply test method

--- spec/math_operations_spec.rb
+++ spec/math_operations_spec.rb
@@ -15,4 +15,11 @@ describe MathOperations do
             expect(result.subtract(5, 2)).to eql(3)
         end
     end
+
+    describe '#multiply' do
+        it 'returns the product of two numbers' do
+            result = MathOperations.new
+            expect(result.multiply(2, 3)).to eql(6)
+        end
+    end
 end
