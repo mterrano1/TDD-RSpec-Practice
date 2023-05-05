d6cc06ee71b53da4df3b38fff14e683ade6d1ef3
Christopher Terrano <mterrano1@yahoo.com>
2023-05-04 23:21:13 -0500

    create divide test method

--- spec/math_operations_spec.rb
+++ spec/math_operations_spec.rb
@@ -22,4 +22,16 @@ describe MathOperations do
             expect(result.multiply(2, 3)).to eql(6)
         end
     end
+
+    describe '#divide' do
+        it 'returns the quotient of two numbers' do
+            result = MathOperations.new
+            expect(result.divide(6, 2)).to eql(3)
+        end
+
+        it 'raises an error when dividing by zero' do
+            result = MathOperations.new
+            expect { result.divide(6, 0) }.to raise_error(ZeroDivisionError)
+        end
+    end
 end
